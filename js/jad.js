4cd9b37ae92a10d1b996009ad7a21680300a0117
Jay <>
2023-08-06 15:46:56 +0300

    Jad: changed jad.js edited variables changed let to const

--- js/jad.js
+++ js/jad.js
@@ -1,15 +1,15 @@
 // PAGES
-let home             = document.getElementById("home");
-let about            = document.getElementById("about");
-let gallery          = document.getElementById("gallery");
-let services         = document.getElementById("services");
-let contact          = document.getElementById("contact");
+const home             = document.getElementById("home");
+const about            = document.getElementById("about");
+const gallery          = document.getElementById("gallery");
+const services         = document.getElementById("services");
+const contact          = document.getElementById("contact");
 // BUTTONS
-let home_button      = document.getElementById("home_button");
-let about_button     = document.getElementById("about_button");
-let gallery_button   = document.getElementById("gallery_button");
-let services_button  = document.getElementById("services_button");
-let contact_button   = document.getElementById("contact_button");
+const home_button      = document.getElementById("home_button");
+const about_button     = document.getElementById("about_button");
+const gallery_button   = document.getElementById("gallery_button");
+const services_button  = document.getElementById("services_button");
+const contact_button   = document.getElementById("contact_button");
 
 // functions
 home_button.onclick = function() {
